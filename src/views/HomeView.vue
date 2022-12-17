f4a1ca219cbc9e088683ed93a1540fe85f5d7202
Alassane Ndoye <aloutndoye.an@gmail.com>
2022-12-17 02:12:21 +0000

    single file component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,62 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>Events</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from "@/components/EventCard.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    EventCard,
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 123,
+          category: "animal welfare",
+          title: "Cat Adoption Day",
+          description: "Find your new feline friend at this event.",
+          location: "Meow Town",
+          date: "January 28, 2022",
+          time: "12:00",
+          organizer: "Kat Laydee",
+        },
+        {
+          id: 456,
+          category: "food",
+          title: "Community Gardening",
+          description: "Join us as we tend to the community edible plants.",
+          location: "Flora City",
+          date: "March 14, 2022",
+          time: "10:00",
+          organizer: "Fern Pollin",
+        },
+        {
+          id: 789,
+          category: "sustainability",
+          title: "Beach Cleanup",
+          description: "Help pick up trash along the shore.",
+          location: "Playa Del Carmen",
+          date: "July 22, 2022",
+          time: "11:00",
+          organizer: "Carey Wales",
+        },
+      ],
+    };
   },
 };
 </script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
