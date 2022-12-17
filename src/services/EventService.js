f24123be9d51f1c1037c36f71e7562a4cfcbcc2d
Alassane Ndoye <aloutndoye.an@gmail.com>
2022-12-17 16:50:07 +0000

    API call with axios

---
+++ src/services/EventService.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL:
+    "https://my-json-server.typicode.com/aloundoye/real-world-vue/events",
+  withCredentials: false,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+export default {
+  getEvents() {
+    return apiClient.get("/events");
+  },
+};
