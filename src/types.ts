92a34cba067266868d7bd73c478752b81322de4e
Alassane Ndoye <aloutndoye.an@gmail.com>
2022-12-20 16:59:07 +0000

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+//contains all the types for the data that will be used in the application
+
+export interface EventItem {
+  id: number;
+  category: string;
+  title: string;
+  description: string;
+  location: string;
+  date: string;
+  time: string;
+  organizer: string;
+}
